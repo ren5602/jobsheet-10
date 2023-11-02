6bf18cd192d08a307b7571dfc294c80dacc0cbf6
ren5602 <naziz5602@gmail.com>
2023-11-02 14:29:58 +0700

    bioskop

---
+++ BioskopWithScanner.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class BioskopWithScanner {
+public static void main(String[] args) {
+    Scanner input= new Scanner(System.in);
+    int baris, kolom;
+    String nama, next;
+
+    String[][] penonton = new String[4][2];
+    while (true) {
+        System.out.print("masukkan nama penonton: ");
+        nama = input.nextLine();
+        System.out.print("masukkan baris: ");
+        baris = input.nextInt();
+        System.out.print("masukkan kolom: ");
+        kolom = input.nextInt();
+        input.nextLine();
+
+        penonton[baris-1][kolom-1] = nama;
+
+        System.out.println("apakah ada penonton lain? (y/n)");
+        next = input.nextLine();
+        if (next.equalsIgnoreCase("n")) {
+            break;            
+        }
+        
+    }
+}    
+}
